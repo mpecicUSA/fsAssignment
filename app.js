cad3cc055ace35451d20040988fb872d87c1302e
Marko Pecic <mpecic2010@gmail.com>
2018-10-25 10:21:02 -0700

    done2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 // ## Challenge 1:
 // Simply print the contents of the text file to the screen.
-  let readFile = fs.readFile("./challenge1/info.txt",'utf8', (data,err)=>{
+  let readFile = fs.readFile("./challenge1/info.txt",'utf8', (err, data)=>{
     if (err){
       console.log(err)
     } else {
@@ -14,10 +14,10 @@ var fs = require('fs');
   })
 
   console.log(readFile)
-// ## Challenge 2:
-// Add the names of everyone in your group to the end of the text file.
+// // ## Challenge 2:
+// // Add the names of everyone in your group to the end of the text file.
   let names = ["Marko", "Nathan", "Marcus", "Harrison", "Nick", "Dennis", "Lucas", "Greg", "Oscar", "Sam"];
-    fs.appendFile("./challenge2/info.txt", names, (data, err)=>{
+    fs.appendFile("./challenge2/info.txt", names, (err, data)=>{
       if(err){
         console.log(err)
       }else{
@@ -25,32 +25,32 @@ var fs = require('fs');
         }
       });
 
-// ## Challenge 3:
-// The file was incorrectly named binfo.txt ... Rename it to info.txt (only using js!)
-  fs.rename("./challenge3/binfo.txt", "./challenge3/info.txt", (data,err)=>{
+// // ## Challenge 3:
+// // The file was incorrectly named binfo.txt ... Rename it to info.txt (only using js!)
+  fs.rename("./challenge3/binfo.txt", "./challenge3/info.txt", (err,data)=>{
     if (err){
       console.log(err)
     }else {
       console.log(data);
     }
     });
-// ## Challenge 4:
-// Create a new folder within the challenge4 folder called "copyfolder". Then copy info.txt into that folder.
-  // fs.mkdir("./challenge4/copyfolder", (data, err) => {
-  //   if(err){
-  //     console.log(err);
-  //   }else{
-  //     fs.copyFile("./challenge4/info.txt", "./challenge4/copyfolder/info.txt", (data,err) {
-  //       if(err){
-  //         console.log(err);
-  //       } else{
-  //         console.log(data);
-  //       }
-  //     });
-  //   }
-  // });
-// ## Challenge 5:
-// Replace all of the "-"'s inside the text file with spaces.
+// // ## Challenge 4:
+// // Create a new folder within the challenge4 folder called "copyfolder". Then copy info.txt into that folder.
+  fs.mkdir("./challenge4/copyfolder", (err, data) => {
+    if(err){
+      console.log(err);
+    }else{
+      fs.copyFile("./challenge4/info.txt", "./challenge4/copyfolder/info.txt", (err,data) =>{
+        if(err){
+          console.log(err);
+        } else{
+          console.log(data);
+        }
+      });
+    }
+  });
+// // ## Challenge 5:
+// // Replace all of the "-"'s inside the text file with spaces.
   let data = fs.readFileSync("./challenge5/info.txt", "utf8");
       let newData = "";
       for(i=0; i < data.length; i++){
@@ -63,3 +63,19 @@ var fs = require('fs');
   fs.writeFileSync('./challenge5/info.txt', newData);
 // ## Challenge 6:
 // Display all files that end with ".txt". Don't hard code the file names!
+let files;
+  fs.readdir("./challenge6/", (err, data)=>{
+    if(err){
+      console.log(err);
+    }else{
+      files = data;
+      let newFiles=[];
+      for(let i = 0; i < files.length; i++){
+        if(files[i].endsWith(".txt")===true){
+          newFiles.push(files[i]);
+          // console.log(newFiles);
+      }
+    }
+    console.log(newFiles);
+    }
+  });
